132e38dd3990ae3740114b64af5d1f905ca02e5d
shitalbansode <shitalbansode9890@gmail.com>
2022-03-30 17:14:14 +0530

    initial commit

---
+++ SpringBootFinalGitApplicaton/src/main/java/com/csi/SpringBootFinalGitApplicatonApplication.java
@@ -0,0 +1,13 @@
+package com.csi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFinalGitApplicatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFinalGitApplicatonApplication.class, args);
+	}
+
+}
